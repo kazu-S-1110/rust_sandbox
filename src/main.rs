123c629fe5c8ba7967c1bec281d11fbd8283d278
kazu-S-1110 <xtpgdmw3690@gmail.com>
2022-06-27 00:34:08 +0900

    複数回ファイル書き込みすることはできた。

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,30 @@ use std::sync::{Arc, Mutex};
 use std::thread;
 
 fn main() {
-    let mut file = File::create("buffer.txt").unwrap();
-    let target = Arc::new(Mutex::new(file));
+    let target = Arc::new(Mutex::new(File::create("buffer.txt").unwrap()));
     let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
 
-    let th1 = thread::spawn(move || {
+    for _ in 0..10 {
         let counter = Arc::clone(&counter);
         let target = Arc::clone(&target);
-        let mut num = counter.lock().unwrap();
-        let mut target_file = target.lock().unwrap();
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            let mut target_file = target.lock().unwrap();
 
-        let content = format!("th1 : {}", num);
+            let content = format!("thread A: {} \n", num);
 
-        let f = write!(target_file, "{}", content);
-        let _res = match f {
-            Ok(file) => file,
-            Err(error) => panic!("why rust {:?}", error),
-        };
-        *num += 1;
-    });
+            let f = write!(target_file, "{}", content);
+            let _res = match f {
+                Ok(file) => file,
+                Err(error) => panic!("why rust {:?}", error),
+            };
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
 }
