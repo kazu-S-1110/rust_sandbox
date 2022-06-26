424ccaad7670a07155d2d3026e30091e8c7bc7a8
kazu-S-1110 <xtpgdmw3690@gmail.com>
2022-06-27 00:45:39 +0900

    複数スレッドで書き込むことには成功。

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::fmt::format;
 use std::fs::File;
 use std::io::prelude::*;
 use std::sync::{Arc, Mutex};
@@ -6,29 +5,41 @@ use std::thread;
 
 fn main() {
     let target = Arc::new(Mutex::new(File::create("buffer.txt").unwrap()));
-    let counter = Arc::new(Mutex::new(0));
-    let mut handles = vec![];
+    let mut handles1 = vec![];
+    let mut handles2 = vec![];
 
-    for _ in 0..10 {
-        let counter = Arc::clone(&counter);
+    for x in 0..10 {
         let target = Arc::clone(&target);
-        let handle = thread::spawn(move || {
-            let mut num = counter.lock().unwrap();
+        let handle1 = thread::spawn(move || {
             let mut target_file = target.lock().unwrap();
-
-            let content = format!("thread A: {} \n", num);
+            let content = format!("thread A: {} \n", x);
 
             let f = write!(target_file, "{}", content);
             let _res = match f {
                 Ok(file) => file,
                 Err(error) => panic!("why rust {:?}", error),
             };
-            *num += 1;
         });
-        handles.push(handle);
+
+        handles1.push(handle1);
     }
 
-    for handle in handles {
-        handle.join().unwrap();
+    for x in 0..5 {
+        let target = Arc::clone(&target);
+        let handle2 = thread::spawn(move || {
+            let mut target_file = target.lock().unwrap();
+
+            let content = format!("thread B: {} \n", x);
+
+            let f = write!(target_file, "{}", content);
+            let _res = match f {
+                Ok(file) => file,
+                Err(error) => panic!("why rust {:?}", error),
+            };
+        });
+        handles2.push(handle2);
     }
+
+    handles1.into_iter().for_each(|h| h.join().unwrap());
+    handles2.into_iter().for_each(|h| h.join().unwrap());
 }
