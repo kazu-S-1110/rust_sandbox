35a90f16bfc014da57321df672dc90bac1baa7d3
kazu-S-1110 <xtpgdmw3690@gmail.com>
2022-06-26 22:49:40 +0900

    チャンネルを使ってデータを共有。

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
-use std::thread;
+use std::{sync::mpsc, thread};
 
 fn main() {
-    let x = vec![100, 200];
-    thread::spawn(move || println!("x is {:?}", x));
+    let (sender1, receiver1) = mpsc::channel();
+    let (sender2, receiver2) = mpsc::channel();
 
-    println!("ok")
+    thread::spawn(move || {
+        let val = receiver1.recv().unwrap();
+        println!("send from main thread. {}", val);
+
+        sender2.send("hi".to_string()).unwrap();
+    });
+
+    sender1.send("hello".to_string()).unwrap();
+
+    let val = receiver2.recv().unwrap();
+    println!("send from sub thread. {}", val);
+
+    // println!("x is {:?}", x) 所有権が写ってしまったので実行不可。
 }
